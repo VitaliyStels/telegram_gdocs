5180183593571b36de1f6395b6955023767c30a9
VitaliyStels <38788206+VitaliyStels@users.noreply.github.com>
2023-11-03 01:29:42 +0200

    Added participation button, Documentation updates

--- README.md
+++ README.md
@@ -3,10 +3,26 @@ The key goal of project to get the user ID of giveaway participants from Telegra
 
 ## Get started
 
-Installation
+### Installation
 ```shell
 pip install -r requirements.txt
 ```
 
+### Setting up
+create .env file
+and fill up required fields
+
+```
+SPREADSHEET_ID="targeted_spreadsheet_id"
+RANGE_NAME="targeted_range"
+TELEGRAM_API_TOKEN="telegram_api_token"
+CHANNEL_ID="telegram_channel_id"
+```
+
+
+## Using
+1. Add the bot as administrator of telegram channel
+2. The bility to send messages is required
+3. Post the /init message on the channel to trigger the bot (This message can be deleted right after bot has started)
 
 All the key data is stored in folder 'data'
--- telegramParser.py
+++ telegramParser.py
@@ -1,7 +1,28 @@
-import json
+from dotenv import load_dotenv
+import os
+import telebot
 
-usersData = []
-with open('data/usersData.json', 'r') as file:
-    usersData = json.load(file)
+load_dotenv()
 
-print(usersData)
+bot = telebot.TeleBot(os.getenv('TELEGRAM_API_TOKEN'))
+CHANNEL_ID = os.getenv('CHANNEL_ID')
+
+def writeUsersData(user):
+    with open('data/users.txt', 'a') as f:
+        f.write(f"{user.username} {user.id}")
+
+@bot.message_handler(commands=['init'])
+def handle_message(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    giveAwayButton = telebot.types.InlineKeyboardButton(text='Участвовать', callback_data='participate')
+    keyboard.add(giveAwayButton)
+    bot.send_message(CHANNEL_ID, 'Хотите участвовать?', reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda c: True)
+def handle_callback_query(callback_query):
+    if callback_query.data == 'participate':
+        writeUsersData(callback_query.from_user)
+        bot.send_message(callback_query.from_user.id, 'Вы успешно добавили свое имя в список участников!')
+
+if __name__ == '__main__':
+    bot.polling()
